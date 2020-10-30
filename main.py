4e8874c1071198faa4ed5823fc04691ba1e8c833
Sterbehilfe92 <yassir.machaka@web.de>
2020-10-30 23:10:07 +0100

    Add Prefix Edit requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import discord
+from discord.ext import commands
 
 with open('token.secret')as file:
     TOKEN = file.read().strip()
 
-client = discord.client()
 
+def get_prefix(bot, message):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+
+    # Notice how you can use spaces in prefixes. Try to keep them simple though.
+    prefix = ['l!', 'L!', '.', 'll!', 'LL!', 'Ll!', 'lL!']
+
+    # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
+    return commands.when_mentioned_or(*prefix)(bot, message)
+
+
+client = commands.Bot(command_prefix=get_prefix)
 
 client.run(TOKEN)
-
