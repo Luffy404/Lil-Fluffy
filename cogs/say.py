e19e4f46acec9f341b3768880020ca6363f0198c
Sterbehilfe92 <yassir.machaka@web.de>
2020-11-08 02:10:43 +0100

    Add description to say command

--- cogs/say.py
+++ cogs/say.py
@@ -6,8 +6,12 @@ class Say(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(brief="Sends a Text in the name of the Bot.", usage=["hello world!"])
     async def say(self, ctx, *, message):
+        """
+        Sends a Text message in the name of the Bot.
+        BEWARE: Messages get Logged in case of Abusal!
+        """
         await ctx.message.delete()
         await ctx.send(message)
 
