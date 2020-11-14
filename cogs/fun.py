5fc719e2b1741406b19d664a7a8012cbb7a05753
Sterbehilfe92 <yassir.machaka@web.de>
2020-11-14 19:38:54 +0100

    Add Commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -23,6 +23,46 @@ class Fun(commands.Cog):
             embed.description = f'{member.mention}'
         await ctx.send(embed=embed)
 
+    @commands.command(brief='Shows a neko gif!')
+    async def ngif(self, ctx):
+        """
+        Shows an Gif about a Neko
+        """
+        data = await requests.get_data('https://nekos.life/api/v2/img/ngif')
+        image_url = data["url"]
+        embed = discord.Embed().set_image(url=image_url) \
+            .set_footer(icon_url=ctx.message.author.avatar_url, text=f"Requested by {ctx.message.author.name}")
+        embed.title = 'Meow!'
+        await ctx.send(embed=embed)
+
+    @commands.command(brief='CATS EVERYWHERE', aliases=['cat'])
+    async def meow(self, ctx):
+        """
+        Shows a cute cat pic!
+        """
+        data = await requests.get_data('https://nekos.life/api/v2/img/meow')
+        image_url = data["url"]
+        embed = discord.Embed().set_image(url=image_url) \
+            .set_footer(icon_url=ctx.message.author.avatar_url, text=f"Requested by {ctx.message.author.name}")
+        embed.title = 'Meow!'
+        await ctx.send(embed=embed)
+
+    @commands.command(brief='Tickle a User!', usage='@Mad Luffy#3039')
+    async def tickle(self, ctx, member: discord.Member = None):
+        """
+        Tickle someone like it said before.
+        """
+        data = await requests.get_data('https://nekos.life/api/v2/img/meow')
+        image_url = data["url"]
+        embed = discord.Embed().set_image(url=image_url) \
+            .set_footer(icon_url=ctx.message.author.avatar_url, text=f"Requested by {ctx.message.author.name}")
+        embed.title = f'Tickle!'
+        if member is not None:
+            embed.description = f'{ctx.message.author.mention} has tickled {member.mention}!'
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Select a user!")
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
