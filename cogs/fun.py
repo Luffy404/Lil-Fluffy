1397a4846423967a8723647ecb8ca7e405995ac8
Sterbehilfe92 <yassir.machaka@web.de>
2020-11-14 05:14:49 +0100

    Add baka Command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,27 @@
-class Fun:
+import discord
+from discord.ext import commands
+
+from cogs.internal import requests
+
+
+class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(brief='Call someone an idiot!', usage='@Mad Luffy#3039')
+    async def baka(self, ctx, member: discord.Member = None):
+        """
+        Call someone an idiot
+        """
+        data = await requests.get_data('https://nekos.life/api/v2/img/baka')
+        image_url = data["url"]
+        embed = discord.Embed()
+        embed.title = 'BAKA!'
+        if member is not None:
+            embed.description = f'{member.mention}'
+        embed.set_image(url=image_url)
+        embed.set_footer(icon_url=ctx.message.author.avatar_url, text=f"Requested by {ctx.message.author.name}")
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
