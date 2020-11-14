e3c1853e3c9881c1a3ba3d351d558172fdf88291
Sterbehilfe92 <yassir.machaka@web.de>
2020-11-14 04:43:01 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Lil-Fluffy-V3',
+    version='PRE-RELEASE',
+    packages=[''],
+    url='https://github.com/Luffy404/Lil-Fluffy',
+    license='MIT',
+    author='Luffy404',
+    author_email='',
+    description='This Bot is created by Luffy404 and can execute various Tasks'
+)
