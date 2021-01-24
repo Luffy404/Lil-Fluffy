aa6a1f5f14ac1cefbbbf84acc3a8b36ef3ffb6f5
Sterbehilfe92 <yassir.machaka@web.de>
2021-01-24 02:35:02 +0100

    Add: More File Formats to count in config.json Rewrite: setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open('README.md') as fp:
+    long_description = fp.read()
+
+
+def parse_requirements_file():
+    with open("requirements.txt") as fp:
+        dependencies = (d.strip() for d in fp.read().split("\n") if d.strip())
+        return [d for d in dependencies if not d.startswith("#")]
+
+
+setuptools.setup(
     name='Lil-Fluffy-V3',
-    version='PRE-RELEASE',
-    packages=[''],
-    url='https://github.com/Luffy404/Lil-Fluffy',
-    license='MIT',
+    version='BETA',
     author='Luffy404',
-    author_email='',
-    description='This Bot is created by Luffy404 and can execute various Tasks'
+    description='This Bot is created by Luffy404 and can execute various Tasks',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Luffy404/Lil-Fluffy',
+    packages=setuptools.find_packages,
+    include_package_data=True,
+    install_requires=parse_requirements_file(),
+    python_requires='>=3.9',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.9',
+    ]
 )
